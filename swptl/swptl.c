ddc866ab763a805282a48a021037d8862b7915ba
Camille Gouraud <camille.gouraud@eviden.com>
2024-07-18 11:58:42 +0200

    Fix the value of the rank if the interface is logical

--- swptl/swptl.c
+++ swptl/swptl.c
@@ -1274,6 +1274,20 @@ void swptl_trig(struct swptl_ni *ni)
 			sodata->u.ictx = trig->u.tx.ictx;
 			sodata->conn = bximsg_getconn(ni->dev->iface, trig->u.tx.nid,
 						      trig->u.tx.pid, ni->vc);
+
+			/*
+			* If rank is not set (i.e. setmap() not called when
+			* connection was accepted), then try to set it in case
+			* setmap() was called meanwhile. If the rank is still not
+			* known then drop the packet.
+			*/
+			if (!SWPTL_ISPHYSICAL(ni->vc)) {
+				sodata->conn->rank = swptl_ni_p2l(ni, sodata->conn->nid, sodata->conn->pid);
+				if (sodata->conn->rank == -1) {
+					LOGN(2, "%s: rank not set\n", __func__);
+				}
+			}
+		
 			ctx = &sodata->u.ictx;
 			swptl_volmove(ctx);
 			sodata->hdrsize = swptl_qhdr_getsize(ctx->cmd);
@@ -2277,6 +2291,20 @@ int swptl_cmd(int cmd, struct swptl_ni *ni, struct swptl_md *get_md, ptl_size_t 
 		sodata = pool_get(&ni->ictx_pool);
 		sodata->init = 1;
 		sodata->conn = bximsg_getconn(ni->dev->iface, nid, pid, ni->vc);
+		
+		/*
+		* If rank is not set (i.e. setmap() not called when
+		* connection was accepted), then try to set it in case
+		* setmap() was called meanwhile. If the rank is still not
+		* known then drop the packet.
+		*/
+		if (!SWPTL_ISPHYSICAL(ni->vc)) {
+			sodata->conn->rank = swptl_ni_p2l(ni, sodata->conn->nid, sodata->conn->pid);
+			if (sodata->conn->rank == -1) {
+				LOGN(2, "%s: rank not set\n", __func__);
+				return 0;
+			}
+		}
 		ctx = &sodata->u.ictx;
 	}
 
@@ -3089,7 +3117,6 @@ int swptl_rcv_rstart(struct swptl_ni *ni, struct swptl_reply *reply, int nid, in
 
 	if (reply->cookie >= SWPTL_ICTX_COUNT)
 		ptl_panic("%d: bad reply cookie %p\n", reply->serial, reply);
-
 	f = *pctx = (struct swptl_sodata *)ni->ictx_pool.data + reply->cookie;
 	f->hdrsize = offsetof(struct swptl_hdr, u) + sizeof(struct swptl_reply);
 	ctx = &f->u.ictx;
