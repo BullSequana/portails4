ae649c04c0f063a518f024aa682f2500943247b5
Camille Gouraud <camille.gouraud@eviden.com>
2024-07-23 09:50:22 +0200

    Add put_to_self_logical example

--- examples/README.md
+++ examples/README.md
@@ -8,6 +8,8 @@ In this folder, you can find examples where the Portals functions are used.
 
 * **put_to_self** : show the sending of a message to oneself and handle completion events.
 
+* **put_to_self_logical** : desmonstrate the sending of a message and its ack using a logical interface. 
+
 * **hello** : send an hello world from a client to a server (physical addressing). See the source files for a detailed usage.
     - client : send a message and receive it's ack. Usage: `client <server nid> <message to send>`
     - server : receive the message and read it. Usage `server [number of messages to wait]`
---
+++ examples/put_to_self_logical.c
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) Bull S.A.S - 2024
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * BXI Low Level Team
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "portals4.h"
+#include "portals4_ext.h"
+
+/*
+ * This example shows a PUT data transfer to ourselves with a logical interface
+ */
+
+int main(void)
+{
+	int res = 0;
+	int ret;
+	char msg[PTL_EV_STR_SIZE];
+	ptl_handle_ni_t nih;
+	ptl_handle_eq_t eqh;
+	ptl_index_t pti;
+	ptl_handle_le_t leh;
+	ptl_process_t id;
+	ptl_process_t mapping[1];
+	ptl_event_t ev;
+	ptl_handle_md_t mdh;
+	ptl_process_t rank0;
+	ptl_le_t le;
+	ptl_md_t md;
+	char data[] = "Hello, put_to_self logical !";
+	char receive_data[sizeof(data)];
+
+	rank0.rank = 0;
+
+	ret = PtlInit();
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlInit failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		return 1;
+	}
+
+	ret = PtlNIInit(PTL_IFACE_DEFAULT, PTL_NI_NO_MATCHING | PTL_NI_LOGICAL, PTL_PID_ANY, NULL,
+			NULL, &nih);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlNIInit failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto fini;
+	}
+
+	ret = PtlGetPhysId(nih, &id);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlGetId failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto pt_free;
+	} else {
+		mapping[0] = id;
+	}
+
+	/* set the mapping from our logical id (rank) to our physical id (nid and pid)*/
+	ret = PtlSetMap(nih, 1, mapping);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlSetMap failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto pt_free;
+	}
+
+	ret = PtlEQAlloc(nih, 10, &eqh);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlEQAlloc failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto ni_fini;
+	}
+
+	ret = PtlPTAlloc(nih, 0, eqh, PTL_PT_ANY, &pti);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlPTAlloc failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto eq_free;
+	}
+
+	/* describe the memory used to receive the message */
+	le = (ptl_le_t){ .start = receive_data,
+			 .length = sizeof(data),
+			 .ct_handle = PTL_CT_NONE,
+			 .uid = PTL_UID_ANY,
+			 .options = PTL_LE_OP_PUT };
+
+	/* expose to the network the receive buffer */
+	ret = PtlLEAppend(nih, pti, &le, PTL_PRIORITY_LIST, NULL, &leh);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlLEAppend failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto pt_free;
+	}
+
+	/* wait for the LINK event that confirms we are ready for message reception*/
+	ret = PtlEQWait(eqh, &ev);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlEQWait failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto le_unlink;
+	}
+	PtlEvToStr(0, &ev, msg);
+	printf("Event : %s\n", msg);
+
+	/* describe the memory used to send the message */
+	md = (ptl_md_t){ .start = data,
+			 .length = sizeof(data),
+			 .options = 0,
+			 .eq_handle = eqh,
+			 .ct_handle = PTL_CT_NONE };
+
+	/* create a memory descriptor to be used to send message */
+	ret = PtlMDBind(nih, &md, &mdh);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlMDBind failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto le_unlink;
+	}
+
+	/* send the message to ourself */
+	ret = PtlPut(mdh, 0, sizeof(data), PTL_ACK_REQ, rank0, pti, 0, 0, NULL, 0);
+	if (ret != PTL_OK) {
+		fprintf(stderr, "PtlPut failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+		res = 1;
+		goto md_release;
+	}
+
+	/* Wait for the send, ack & put events */
+	for (int i = 0; i < 3; i++) {
+		ret = PtlEQWait(eqh, &ev);
+		if (ret != PTL_OK) {
+			fprintf(stderr, "PtlEQWait failed : %s \n", PtlToStr(ret, PTL_STR_ERROR));
+			res = 1;
+			goto md_release;
+		}
+		PtlEvToStr(0, &ev, msg);
+		printf("Event : %s \n", msg);
+	}
+
+	printf("Message received : %s \n", (char *)md.start);
+
+md_release:
+	PtlMDRelease(mdh);
+le_unlink:
+	PtlLEUnlink(leh);
+pt_free:
+	PtlPTFree(nih, pti);
+eq_free:
+	PtlEQFree(eqh);
+ni_fini:
+	PtlNIFini(nih);
+fini:
+	PtlFini();
+
+	return res;
+}
